bb5a4f76b2cab0eff9b2a41af9f44784b20ebdf5
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2016-06-15 13:20:09 +0200

    Added integration example for Spock and Geb.

---
+++ spock-geb/src/test/groovy/de/qaware/showcase/spock/geb/HomePageSpec.groovy
@@ -0,0 +1,64 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 QAware GmbH, Munich, Germany
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package de.qaware.showcase.spock.geb
+
+import geb.spock.GebReportingSpec
+import spock.lang.Stepwise
+
+import static org.hamcrest.CoreMatchers.containsString
+import static org.junit.Assert.assertThat
+
+/**
+ * A simple Spock specification to test the QAware home page.
+ */
+@Stepwise
+class HomePageSpec extends GebReportingSpec {
+
+    def "Launch browser and navigate to index page"() {
+        when: 'we navigate to the QAware homepage'
+        go("http://www.qaware.de")
+
+        then: 'the index page is displayed with the correct headline'
+        waitFor { at IndexPage }
+        assertThat headline.text(), containsString("Qualität und Agilität")
+    }
+
+    def "Navigate to the career page"() {
+        when: 'we navigate to the career page'
+        to CareerPage
+
+        then: 'the career page is displayed with the correct headline'
+        waitFor { at CareerPage }
+        assertThat headline.text(), containsString("Erfindergeist und Handwerksstolz.")
+    }
+
+    def "Navigate to the community page"() {
+        when: 'we navigate to the community page'
+        to CommunityPage
+
+        then: 'the community page is displayed with the correct headline'
+        waitFor { at CommunityPage }
+        assertThat headline.text(), containsString("IT-Community in der Welt")
+    }
+}
